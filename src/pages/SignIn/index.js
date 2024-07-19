f0097ad99aa22a2e01bb719a6276e3b15d0ec0e3
Yogi Hua <190710439@students.uajy.ac.id>
2024-07-20 00:52:52 +0700

    20-7-2024

--- src/pages/SignIn/index.js
+++ src/pages/SignIn/index.js
@@ -1,19 +1,40 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Gap, Header, TextInput} from '../../components';
+import {useForm} from '../../utils';
 
 const SignIn = ({navigation}) => {
+  // const [email, setEmail] = useState('');
+  // const [password, setPassword] = useState('');
+  const [form, setForm] = useForm({
+    email: '',
+    password: '',
+  });
+
+  const onSubmit = () => {
+    console.log('form: ', form);
+  };
+
   return (
     <View style={styles.page}>
       <Header title="Sign In" subTitle="Find you best ever meal"></Header>
       <View style={styles.container}>
         <TextInput
           label="Email address"
-          placeholder="email@gmail.com"></TextInput>
+          placeholder="email@gmail.com"
+          value={form.email}
+          onChangeText={value => setForm('email', value)}
+        />
         <Gap height={16}></Gap>
-        <TextInput label="Password" placeholder="xxxxxxxxx"></TextInput>
+        <TextInput
+          label="Password"
+          placeholder="xxxxxxxxx"
+          value={form.password}
+          onChangeText={value => setForm('password', value)}
+          secureTextEntry
+        />
         <Gap height={24}></Gap>
-        <Button text="Sign In"></Button>
+        <Button text="Sign In" onPress={onSubmit} />
         <Gap height={12}></Gap>
         <Button
           text="Create new account"
